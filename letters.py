97c614311416ade0aa602bb8caac66ba6370ef85
mattindy <fmindiveri@gmail.com>
2017-07-06 19:02:51 +0200

    Add files via upload

---
+++ letters.py
@@ -0,0 +1,17 @@
+letters = [ 'a' , 'b' , 'c' , 'd' , 'e']
+letters.remove ( 'c')
+print letters
+
+letters = [ 'a' , 'b' , 'c' , 'd' , 'e']
+del letters [3]
+print letters
+
+letters = [ 'a' , 'b' , 'c' , 'd' , 'e']
+lastLetter = letters.pop()
+print letters
+print lastLetter
+
+letters = [ 'a' , 'b' , 'c' , 'd' , 'e']
+second = letters.pop(1)
+print second
+print letters
